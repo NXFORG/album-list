6b7309884acbfea10fa48855df379562a74fd2cd
“nxforg” <christopher.anstead@yahoo.co.uk>
2021-11-03 17:56:19 +0000

    unclickable likes and dislikes

--- src/stories/assets/likeCounter/counter.jsx
+++ src/stories/assets/likeCounter/counter.jsx
@@ -3,18 +3,31 @@ import './counter.css';
 //import PropTypes from 'prop-types';
 
 export const Counter = () => {
+    const [ liked, setLiked ] = useState(true);
+    const [ disliked, setDisliked ] = useState(true);
+
     const [ likeValue, setLikeValue ] = useState(0);
     const [ dislikeValue, setDislikeValue ] = useState(0);
 
-    const incrementLikeValue = () => setLikeValue((prevLikes) => prevLikes + 1);
-    const incrementDislikeValue = () => setDislikeValue((prevDislikes) => prevDislikes + 1);
+    const updateLikes = () => {
+        setLiked((prevState) => !prevState);
+        liked ? setLikeValue((prevLikes) => prevLikes + 1) : setLikeValue((prevLikes) => prevLikes - 1); 
+    }
+
+    const updateDislikes = () => {
+        setDisliked((prevState) => !prevState);
+        disliked ? setDislikeValue((prevDislikes) => prevDislikes + 1) : setDislikeValue((prevDislikes) => prevDislikes - 1); 
+    }
+
 
     return (
-        <form>
-            <button value="👍" onClick={incrementLikeValue} disabled={dislikeValue || likeValue > 0 ? true : false }>👍</button>
+        <>
+            {/* <button value="👍" onClick={incrementLikeValue} disabled={dislikeValue || likeValue > 0 ? true : false }>👍</button> */}
+            <button value="👍" onClick={updateLikes} >👍</button>
             <span>{ likeValue }</span>
-            <button value="👎" onClick={incrementDislikeValue} disabled={likeValue || dislikeValue > 0 ? true : false }>👎</button>
+            {/* <button value="👎" onClick={incrementDislikeValue} disabled={likeValue || dislikeValue > 0 ? true : false }>👎</button> */}
+            <button value="👎" onClick={updateDislikes} >👎</button>
             <span>{ dislikeValue }</span>
-        </form>
+        </>
     );
   };
